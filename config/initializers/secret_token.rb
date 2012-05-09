524e90bfb4767e7ea3ee2d216fccd534d3d319c4
lifang <santhe_2004@126.com>
2012-05-09 10:24:24 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordPractice::Application.config.secret_token = '0ef401035eafb6dd88329ec45be9a7b5bed62d8f30e71b2a3239aa6e0de698b332c87319fefbe4fd89ea157559540169bb22a0d2f36dce3ae1b03c1bf848d6af'
