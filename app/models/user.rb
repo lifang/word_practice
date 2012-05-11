cb264605091d2ba4b06b6a03f36bfdcb649c3ba2
lifang <santhe_2004@126.com>
2012-05-11 10:18:23 +0800

    update detial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,21 @@
 #encoding: utf-8
 class User < ActiveRecord::Base
+  require 'rexml/document'
+  include REXML
+  
   has_one :user_word_relation
+  
+  REPEAT_STATUS = {"L" => 1 , "L1" => 2, "L2" => 3, "L3" => 4} #复习共四轮
+  REPEAT_NUM = {1 => "L" , 2 => "L1", 3 => "L2", 4 => "L3"}
+  REPEAT_DAY = {"L" => 1, "L1" => 2, "L2" => 4, "L3" => 8} #每一轮的复习间隔时间分别为：L 1天， L1 2天， L2 4天， L3 8天
+  REPEAT_LAST_DAY = {"L" => 0, "L1" => 1, "L2" => 2, "L3" => 4} #每一轮的复习可以推迟的时间分别为：L 0天， L1 1天， L2 2天， L3 4天
+
 
   FROM = {"sina" => "新浪微博", "renren" => "人人网", "qq" => "腾讯网", "kaixin" => "开心网", "baidu" => "百度"}
   TIME_SORT = {:ASC => 0, :DESC => 1}   #用户列表按创建时间正序倒序排列
   U_FROM = {:WEB => 0, :APP => 1} #注册用户来源，0 网站   1 应用
   USER_FROM = {0 => "网站" , 1 => "应用"}
 
-  DEFAULT_PASSWORD = "123456"
 
   #如果用户是第一次登录，则新建用户的背词列表
   def self.init_word_list(category_id)
@@ -26,11 +34,42 @@ class User < ActiveRecord::Base
     user_word_relation = UserWordRelation.find_by_sql(["select id, practice_url
       from user_word_relations where user_id = ?", user_id])
     unless user_word_relation.nil?
+      doc = user_word_relation.open_file
+      all_dates = doc.root.elements["old_words"].elements["all_date"].text
+      leave_dates = []
+      all_dates.split(",").each {|d|
+        leave_dates << d if d.to_date < Time.now
+      } unless all_dates.nil? or all_dates.empty?
       
-      
+      leave_dates.each {|l_d|
+        word_list = doc.root.elements["old_words"].elements[l_d]
+        if word_list.elements["word"].size > 0
+          word_list.each_element {|w|
+            if w.attributes["step"] == REPEAT_STATUS["L"]
+              doc.delete_elament(w.xpath)
+              doc.root.elements["new_words"].add_element("word", 
+                {"id"=>"#{w.attributes["id"]}", "is_error" => "false", "repeat_time" => "0"})
+            else
+              if w.attributes["end_at"].to_date < Time.now
+                w.attributes["step"] = w.attributes["step"] - 1
+                w.attributes["start_at"] = Time.now.strftime("%Y_%m_%d")
+                w.attributes["end_at"] = (Time.now
+                  + REPEAT_LAST_DAY[REPEAT_NUM[w.attributes["step"].to_i]].days).strftime("%Y_%m_%d")
+                w.attributes["is_error"] = "false"
+                w.attributes["repeat_time"] = "0"
+              end
+            end
+          }
+        else
+          doc.delete_element(word_list.xpath)
+        end        
+      } unless leave_dates.blank?
+      path_url = user_word_relation.practice_url.split("/")
+      user_word_relation.user.write_file(doc, path_url[1], "xml", "user_word_xml")
     end
   end
 
+  #写文件
   def write_file(str, path, file_type, super_path)
     dir = "#{Rails.root}/public/#{super_path}"
     Dir.mkdir(dir) unless File.directory?(dir)
--- app/models/user_word_relation.rb
+++ app/models/user_word_relation.rb
@@ -10,45 +10,12 @@ class UserWordRelation < ActiveRecord::Base
       where w.category_id = ? and uwr.status = #{STATUS[:NOMAL]} and uwr.user_id = ? ", category_id, user_id])
   end
 
-  #用户刚开始练习单词之前给每个用户初始化一条记录
-  def self.single_user_word(user_id, category_id)
-    user_word_relation = UserWordRelation.find_by_user_id_and_category_id(user_id, category_id)
-    if user_word_relation.nil?
-      ids_arr = []
-      word_ids = Word.find_by_sql(["select w.id w_id from words w where w.level < #{Word::WORD_LEVEL[:THIRD]}
-        and w.category_id = ?", category_id])
-      unless word_ids.blank?
-        word_ids.each { |w| ids_arr << w.w_id }
-      end
-      user_word_relation = UserWordRelation.create(:user_id => user_id,
-        :nomal_ids => ids_arr.join(","), :category_id => category_id)
-    end
-    return user_word_relation
-  end
-
-  #用户增加已背诵的单词
-  def self.add_recite_word(user_id, word_id, category_id)
-    user_word_relation = UserWordRelation.single_user_word(user_id, category_id)
-    nomal_ids = user_word_relation.nomal_ids.split(",") - ["#{word_id}"] unless user_word_relation.nomal_ids.nil?
-    recite_ids = ""
-    if user_word_relation.recite_ids.nil?
-      recite_ids = word_id
-    else
-      recite_ids = ((user_word_relation.recite_ids.split(",")) | ["#{word_id}"]).join(",")
-    end
-    user_word_relation.update_attributes(:nomal_ids => nomal_ids.join(","), :recite_ids => recite_ids)
-  end
-
-  #用户增加背诵的单词
-  def self.add_nomal_ids(user_id, word_id, category_id)
-    user_word_relation = UserWordRelation.single_user_word(user_id, category_id)
-    nomal_ids = ""
-    if user_word_relation.nomal_ids.nil?
-      nomal_ids = word_id
-    else
-      nomal_ids = (["#{word_id}"] | (user_word_relation.nomal_ids.split(","))).join(",")
-    end
-    user_word_relation.update_attributes(:nomal_ids => nomal_ids)
+  #打开用户正在背诵的词汇表
+  def open_file
+    file=File.open "#{Constant::PUBLIC_PATH}#{self.practice_url}"
+    doc = Document.new(file)
+    file.close
+    return doc
   end
 
 end
